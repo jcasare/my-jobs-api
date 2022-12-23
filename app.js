853b35426579d877775c70c85639b655f81d60e9
jcasare <asarejerry16@gmail.com>
2022-12-23 10:26:15 -0500

    final final commit lol

--- app.js
+++ app.js
@@ -10,14 +10,25 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
-const authMiddleware = require("./middleware/authentication");
+const authenticateUser = require("./middleware/authentication");
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+const limiter = rateLimiter({ windowMs: 15 * 60 * 100, max: 100 });
 
+app.set("trust proxy", 1);
+//middleware to parse jsond
+app.use(limiter);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth0", authRouter);
-app.use("/api/v1/jobs", authMiddleware, jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- models/User.js
+++ models/User.js
@@ -38,8 +38,10 @@ UserSchema.methods.genToken = function () {
     { expiresIn: process.env.JWT_LIFETIME }
   );
 };
+
 UserSchema.methods.comparePassword = async function (candidatePassword) {
   const isMatch = await bcrypt.compare(candidatePassword, this.password);
   return isMatch;
 };
+
 module.exports = mongoose.model("User", UserSchema);
