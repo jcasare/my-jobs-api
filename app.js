6806f751d312fbdc27dbd70fc88f0509daa8fa6e
jcasare <asarejerry16@gmail.com>
2022-12-23 11:12:45 -0500

    final update

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send(`Jay's job Api`);
+});
 // routes
 app.use("/api/v1/auth0", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
